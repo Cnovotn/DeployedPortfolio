66499da56197ecb5559320e7780b47b6a27c3179
Clayton Novotney <claytonnovotney@gmail.com>
2021-07-14 21:56:10 -0700

    added more images and updated the projects section

--- src/pages/Projects/projects.js
+++ src/pages/Projects/projects.js
@@ -12,7 +12,7 @@ export default class Projects extends Component {
         return (
             <div>
                 <Navbar />
-                <div className="page-container">
+                {/* <div className="page-container">
                     <div className="intro-section">
                         <h1 className='section-name'>Projects</h1>
                         <p>
@@ -25,6 +25,85 @@ export default class Projects extends Component {
                             Water Temperature Sensor,
 
                         </p>
+                    </div>
+                </div> */}
+                <div className="page-container">
+                    <div className="grid-container">
+                        <div className="grid-row">
+                            <div className="small-tile tile tile-case">
+                                <a href="#">
+                                    <div className="tile-primary-content">
+                                        <img className="tile-image" src="https://raw.githubusercontent.com/Cnovotn/DeployedPortfolio/master/src/static/ronovoImages/homepage.png" alt=""/>
+                                    </div>
+                                    <div class="tile-secondary-content">
+                                        <h2>Green Point Real Estate</h2>
+                                        <p>Backend Developer</p>
+                                        <h3>A real estate management platform for investors, current tenents, possible tenents, and company employees.</h3>
+                                    </div>
+                                </a>
+                            </div>
+                            <div className="large-tile tile tile-case">
+                                <a href="#">
+                                    <div className="tile-primary-content">
+                                        <img className="tile-image" src="https://raw.githubusercontent.com/Cnovotn/DeployedPortfolio/master/src/static/arrivedImages/ViewPage.png" alt=""/>
+                                    </div>
+                                    <div class="tile-secondary-content">
+                                        <h2>Green Point Real Estate</h2>
+                                        <p>Backend Developer</p>
+                                        <h3>A real estate management platform for investors, current tenents, possible tenents, and company employees.</h3>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        {/* <div className="grid-row">
+                            <div className="small-tile tile"></div>
+                            <div className="small-tile tile"></div>
+                            <div className="small-tile tile"></div>
+                        </div>
+                        <div className="grid-row">
+                            <div className="large-tile tile"></div>
+                            <div className="small-tile tile"></div>
+                        </div> */}
+                        <div>
+                            {/* <ul class="masonry-list">
+                            <li class="tile-case small-tile">
+                                <a href="#">
+                                    <div class="tile-primary-content">
+                                        <img src="https://raw.githubusercontent.com/Cnovotn/DeployedPortfolio/master/src/static/ronovoImages/homepage.png" alt=""/>
+                                    </div>
+                                    <div class="tile-secondary-content">
+                                        <h2>Green Point Real Estate</h2>
+                                        <p>Backend Developer</p>
+                                        <h3>A real estate management platform for investors, current tenents, possible tenents, and company employees.</h3>
+                                    </div>
+                                </a>
+                            </li>
+                            <li class="tile-case large-tile">
+                                <a href="#">
+                                <div class="tile-primary-content">
+                                    <img src="https://raw.githubusercontent.com/Cnovotn/DeployedPortfolio/master/src/static/arrivedImages/ViewPage.png" alt=""/>
+                                </div>
+                                <div class="tile-secondary-content">
+                                    <h2>Green Point Real Estate</h2>
+                                    <p>Backend Developer</p>
+                                    <h3>A real estate management platform for investors, current tenents, possible tenents, and company employees.</h3>
+                                </div>
+                                </a>
+                            </li>
+                            <li class="tile-case small-tile">
+                                <a href="#">
+                                <div class="tile-primary-content">
+                                    <img src="https://raw.githubusercontent.com/Cnovotn/DeployedPortfolio/master/src/static/ronovoImages/homepage.png" alt=""/>
+                                </div>
+                                <div class="tile-secondary-content">
+                                    <h2>Green Point Real Estate</h2>
+                                    <p>Backend Developer</p>
+                                    <h3>A real estate management platform for investors, current tenents, possible tenents, and company employees.</h3>
+                                </div>
+                                </a>
+                            </li>
+                        </ul> */}
+                        </div>
                     </div>
                 </div>
             </div>
