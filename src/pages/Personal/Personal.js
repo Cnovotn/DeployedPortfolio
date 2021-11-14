03da3e694cf396500dfa5505a1304c7908e2dd16
Clayton Novotney <claytonnovotney@gmail.com>
2021-11-14 15:47:18 -0800

    more changes

--- src/pages/Personal/Personal.js
+++ src/pages/Personal/Personal.js
@@ -13,11 +13,12 @@ export default class Personal extends Component {
                                     <img src="https://raw.githubusercontent.com/Cnovotn/DeployedPortfolio/master/src/static/profilepics/SquareGraduation.png" alt="Body Building"/>
                                 </div>
                                 <div className="personal-content-text">
-                                    <h2 className="personal-content-title">The Strength within</h2>
-                                    <h4 className="personal-content-subtitle">Unleash your inner beast</h4>
-                                    <p className="personal-content-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur esse aut deleniti magni. Quaerat unde non beatae hic veniam necessitatibus reiciendis reprehenderit tempore alias maxime delectus, ullam aut adipisci, aliquid velit dolores molestiae, eius nihil nam et ut fugiat iste perspiciatis quis? Veniam accusantium ut recusandae eaque quas laudantium nostrum?</p>
-                                    <p className="personal-content-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur esse aut deleniti magni. Quaerat unde non beatae hic veniam necessitatibus reiciendis reprehenderit tempore alias maxime delectus, ullam aut adipisci, aliquid velit dolores molestiae, eius nihil nam et ut fugiat iste perspiciatis quis? Veniam accusantium ut recusandae eaque quas laudantium nostrum?</p>
-                                    <a href="#" className="btn">View More</a>
+                                    <h2 className="personal-content-title">Graduating College</h2>
+                                    <h4 className="personal-content-subtitle">University Of Washington - B.S. Informatics</h4>
+                                    <p className="personal-content-paragraph">I was born a WSU Coug fan, but will die a Husky. Both of my parents attended Washington State University, and I grew up in a household with crimson and gray everywhere, rooting for them during every Apple Cup, at basketball games, and even during their baseball season. Once I began searching for college opportunities, making the choice of attending UW was easy, and I am beyond grateful about my time in Seattle. </p>
+                                    <p className="personal-content-paragraph">Starting my journey at the UW in the Spring of 2016, I originally wanted to be an Aeronautical Engineer, but after taking an introductory programming class, I had found my calling and began down the path of Software Engineering. I could not wait to start learning more and had not yet been accepted into the Information School at UW, so I began taking  online course that could not quench my thirst. This led me to enrolling at the Coding Dojo's 14 week long bootcamp in Bellevue during the summer of 2018. This gave me my first true taste of the unlimited possibilities that are presented with the technologies available today and I immediately began searching to projects to work on, leading me to my first start up GlimpseWearables as a Full Stack Engineer.</p>
+                                    <p className="personal-content-paragraph">After a painfully long wait, I was admitted into the I-School at UW during the fall of 2019 and began enrolling exclusively in Info courses and began to find my passion for backend and data engineering. While in the I-School, I took many offered electives in advanced database design & modeling, interactive information visualization, data structures and algorithms, data science and many more.</p>
+                                    <a href="/education" className="btn">View More</a>
                                 </div>
                             </div>
                         </div>
@@ -28,10 +29,10 @@ export default class Personal extends Component {
                         <div className="personal-container">
                             <div className="personal-content">
                                 <div className="personal-content-text">
-                                    <h2 className="personal-content-title">The Strength within</h2>
-                                    <h4 className="personal-content-subtitle">Unleash your inner beast</h4>
-                                    <p className="personal-content-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur esse aut deleniti magni. Quaerat unde non beatae hic veniam necessitatibus reiciendis reprehenderit tempore alias maxime delectus, ullam aut adipisci, aliquid velit dolores molestiae, eius nihil nam et ut fugiat iste perspiciatis quis? Veniam accusantium ut recusandae eaque quas laudantium nostrum?</p>
-                                    <p className="personal-content-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur esse aut deleniti magni. Quaerat unde non beatae hic veniam necessitatibus reiciendis reprehenderit tempore alias maxime delectus, ullam aut adipisci, aliquid velit dolores molestiae, eius nihil nam et ut fugiat iste perspiciatis quis? Veniam accusantium ut recusandae eaque quas laudantium nostrum?</p>
+                                    <h2 className="personal-content-title">Big Water Sports Fan</h2>
+                                    <h4 className="personal-content-subtitle">Waterskiing, Surfing, Wakeboarding</h4>
+                                    <p className="personal-content-paragraph">I fell in love with water a very young age and have always had an itch to be in or around it since then. It all started at age 5 when my parents dropped me off at the pool for the swim team that they were excited to see me swim laps with. Meanwhile, I was excited to be in the water and simply swim around, bringing my rubber torpedo with me and chasing it around in the water while everyone else was practicing their form. That was a short lived team involvement for me.</p>
+                                    <p className="personal-content-paragraph">As time progressed, I was fortunate enough to grow up in a neighborhood that had a few wakeboard boats. During the summers, all of the neighbors in our cul-de-sac would go for a camping trip on lake Roosevelt that was full of tubing, skiing, knee boarding, wakeboarding, and eventually surfing when it became popular. I had again, fallen in love with all of the water sport activities and brought this passion to college. During my Sophomore year at the University of Washington, I helped start the "Water Sports Club" that would meet during the spring and summer on Lake Sammamish for all sorts of water sports. We were also fortunate enough to get sponsored by Ronix and Radar, who gave us about $10,000 worth of gear to get started. </p>
                                     <a href="#" className="btn">View More</a>
                                 </div>
                                 <div className="personal-content-img">
@@ -49,10 +50,10 @@ export default class Personal extends Component {
                                     <img src="https://raw.githubusercontent.com/Cnovotn/DeployedPortfolio/master/src/static/profilepics/SkiPatrol.jpeg" alt="Body Building"/>
                                 </div>
                                 <div className="personal-content-text">
-                                    <h2 className="personal-content-title">The Strength within</h2>
-                                    <h4 className="personal-content-subtitle">Unleash your inner beast</h4>
-                                    <p className="personal-content-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur esse aut deleniti magni. Quaerat unde non beatae hic veniam necessitatibus reiciendis reprehenderit tempore alias maxime delectus, ullam aut adipisci, aliquid velit dolores molestiae, eius nihil nam et ut fugiat iste perspiciatis quis? Veniam accusantium ut recusandae eaque quas laudantium nostrum?</p>
-                                    <p className="personal-content-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur esse aut deleniti magni. Quaerat unde non beatae hic veniam necessitatibus reiciendis reprehenderit tempore alias maxime delectus, ullam aut adipisci, aliquid velit dolores molestiae, eius nihil nam et ut fugiat iste perspiciatis quis? Veniam accusantium ut recusandae eaque quas laudantium nostrum?</p>
+                                    <h2 className="personal-content-title">Ski Patrol</h2>
+                                    <h4 className="personal-content-subtitle">Saving Lives Since 2015</h4>
+                                    <p className="personal-content-paragraph">From the time that I could walk, my family got me out in the snow and skiing. They started me out with cross country skiing from ages 3-8, and introduced me to down hill skiing at age 5, which I picked up pretty quickly and have never looked back. Living over in Spokane gave me the opportunity to ski all sorts of mountains with a diverse set of terrain that allowed me to hone in my abilities. </p>
+                                    <p className="personal-content-paragraph">During my junior year of high school, my father and I enrolled in the local volunteer ski patrol at Mount Spokane. We began taking our Outdoor Emergency Care classes at the Spokane Valley Hospital in the spring of 2015, and completed our certification over the course of 10 months at the end of December 2015. This allowed us to begin on mountain protocol training, teaching us the proper ways to respond to, and communicate emergencies over the radio, ski with patients in toboggans, and administer first aid on the mountain with the intent of getting the patient to the next level of care safely and efficiently. Completing our "training" season at the end of 2016 and becoming fully certified alpine patrollers with the National Ski Patrol. Since then, I have patrolled at both Mount Spokane during my winter breaks from the UW, and at Snoqualmie East (Hyak) during the regular season.</p>
                                     <a href="#" className="btn">View More</a>
                                 </div>
                             </div>
@@ -64,8 +65,8 @@ export default class Personal extends Component {
                         <div className="personal-container">
                             <div className="personal-content">
                                 <div className="personal-content-text">
-                                    <h2 className="personal-content-title">The Strength within</h2>
-                                    <h4 className="personal-content-subtitle">Unleash your inner beast</h4>
+                                    <h2 className="personal-content-title">The Outdoors</h2>
+                                    <h4 className="personal-content-subtitle">Blessed to live in the PNW</h4>
                                     <p className="personal-content-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur esse aut deleniti magni. Quaerat unde non beatae hic veniam necessitatibus reiciendis reprehenderit tempore alias maxime delectus, ullam aut adipisci, aliquid velit dolores molestiae, eius nihil nam et ut fugiat iste perspiciatis quis? Veniam accusantium ut recusandae eaque quas laudantium nostrum?</p>
                                     <p className="personal-content-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur esse aut deleniti magni. Quaerat unde non beatae hic veniam necessitatibus reiciendis reprehenderit tempore alias maxime delectus, ullam aut adipisci, aliquid velit dolores molestiae, eius nihil nam et ut fugiat iste perspiciatis quis? Veniam accusantium ut recusandae eaque quas laudantium nostrum?</p>
                                     <a href="#" className="btn">View More</a>
@@ -82,11 +83,11 @@ export default class Personal extends Component {
                         <div className="personal-container">
                             <div className="personal-content">
                                 <div className="personal-content-img">
-                                    <img src="https://raw.githubusercontent.com/Cnovotn/DeployedPortfolio/master/src/static/profilepics/SkiPatrol.jpeg" alt="Body Building"/>
+                                    <img src="https://raw.githubusercontent.com/Cnovotn/DeployedPortfolio/master/src/static/profilepics/FamilyPhoto.jpeg" alt="Body Building"/>
                                 </div>
                                 <div className="personal-content-text">
-                                    <h2 className="personal-content-title">The Strength within</h2>
-                                    <h4 className="personal-content-subtitle">Unleash your inner beast</h4>
+                                    <h2 className="personal-content-title">My Family❤️</h2>
+                                    <h4 className="personal-content-subtitle">I would not be me without them</h4>
                                     <p className="personal-content-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur esse aut deleniti magni. Quaerat unde non beatae hic veniam necessitatibus reiciendis reprehenderit tempore alias maxime delectus, ullam aut adipisci, aliquid velit dolores molestiae, eius nihil nam et ut fugiat iste perspiciatis quis? Veniam accusantium ut recusandae eaque quas laudantium nostrum?</p>
                                     <p className="personal-content-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur esse aut deleniti magni. Quaerat unde non beatae hic veniam necessitatibus reiciendis reprehenderit tempore alias maxime delectus, ullam aut adipisci, aliquid velit dolores molestiae, eius nihil nam et ut fugiat iste perspiciatis quis? Veniam accusantium ut recusandae eaque quas laudantium nostrum?</p>
                                     <a href="#" className="btn">View More</a>
